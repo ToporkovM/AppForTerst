ee21d73a35b12b1edaf061c3c59ff58f634f5d40
max <toporkovmaksim7@gmail.com>
2026-04-01 22:21:18 +0700

    AFK-4 Добавление cocoapods

---
+++ AppForTest/Managers/ApiClient.swift
@@ -0,0 +1,12 @@
+//
+//  ApiClient.swift
+//  AppForTest
+//
+//  Created by Максим on 01.04.2026.
+//
+
+import Alamofire
+
+final class ApiClient {
+    
+}
---
+++ Pods/Target Support Files/Pods-AppForTest-AppForTestUITests/Pods-AppForTest-AppForTestUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppForTest_AppForTestUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AppForTest_AppForTestUITests
+@end
