aeb4207209202fa40e3f8e776bdfd7b4b0afcffe
max <toporkovmaksim7@gmail.com>
2026-04-01 22:12:32 +0700

    AFT-3 Добавление TabBar

--- AppForTest/SupportingFiles/AppDelegate.swift
+++ AppForTest/SupportingFiles/AppDelegate.swift
@@ -9,8 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        setupTabBar()
         return true
+    }
+    
+    private func setupTabBar() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainTabBarController()
+        window?.makeKeyAndVisible()
     }
 }
---
+++ AppForTest/UI/Screens/Main/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  AppForTest
+//
+//  Created by Максим on 01.04.2026.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+        title = "Главная"
+    }
+}
+
